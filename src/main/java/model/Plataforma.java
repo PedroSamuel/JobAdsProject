807e31c1cb133446086d9863f2e77a256d96188c
Pedro Silva <pedrbsilva@gmail.com>
2018-07-13 10:37:12 +0100

    Created classes Plataforma, Entidade e Anuncio Plataforma

--- src/main/java/model/Plataforma.java
+++ src/main/java/model/Plataforma.java
@@ -1,5 +1,36 @@
 package model;
 
-public class Plataforma {
+import java.util.List;
 
+import model.AnuncioPlataforma;
+
+public class Plataforma {
+	String nomePlataforma;
+	int periodoRenovacao;
+	List <AnuncioPlataforma> listaAnuncios;
+	
+	public String getNomePlataforma() {
+		return nomePlataforma;
+	}
+	public void setNomePlataforma(String nomePlataforma) {
+		this.nomePlataforma = nomePlataforma;
+	}
+	public int getPeriodoRenovacao() {
+		return periodoRenovacao;
+	}
+	public void setPeriodoRenovacao(int periodoRenovacao) {
+		this.periodoRenovacao = periodoRenovacao;
+	}
+	public List<AnuncioPlataforma> getListaAnuncios() {
+		return listaAnuncios;
+	}
+	public void setListaAnuncios(List<AnuncioPlataforma> listaAnuncios) {
+		this.listaAnuncios = listaAnuncios;
+	}
+	
+
+
+	
+	
+	
 }
