17f57459b3d4c419e67473c0d3b0f8fcb004abf3
André Simão <amlsimao@gmail.com>
2018-07-17 13:05:10 +0100

    v2

--- src/main/java/control/ControlRequerimento.java
+++ src/main/java/control/ControlRequerimento.java
@@ -1,5 +1,45 @@
 package control;
 
-public class ControlRequerimento {
+import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import model.Anuncio;
+import model.Requerimento;
+import repository.RepositorioAnuncios;
+import repository.RepositorioRequerimentos;
+
+
+@RequestScoped
+public class ControlRequerimento {
+	@Inject
+	RepositorioAnuncios dbR ;
+	
+	public void criarAnuncio(Requerimento requerimento) {
+		dbR.createEntity(requerimento);
+	}
+	
+	public List<Requerimento> Anuncios(){
+		return dbR.listEntity(Requerimento.class);
+	}
+	
+	public Requerimento getRequerimento (Long id) {
+		return dbR.getEntity(Requerimento.class,  id);
+		
+	}
+	
+	public void updateRequerimento(Requerimento r) {
+		dbR.updateEntity(r);
+	}
+	
+	public void removeRequerimento(Requerimento requerimento) {
+		dbR.removeEntity(requerimento);
+		
+		updateList();
+	}
+	
+	public void updateList() {
+		dbR.updateLocalList();
+	}
 }
