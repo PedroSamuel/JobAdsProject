861e0418651392cf7b7e5c28bdb3a1eb52a6f446
André Simão <amlsimao@gmail.com>
2018-08-01 15:36:47 +0100

    .inicio users

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,64 @@
 package model;
 
-public class User {
+import java.io.Serializable;
+
+public class User extends Entidade implements Serializable {
+	
+	private static final long serialVersionUID = -4656759219348212715L;
+
+	  
+	  
+	  
+	  private String username;
+	  
+	  private String email;
+	  
+	  private String password;
+	  
+	  private String salt;
+
+	
+	public String getUsername() {
+		return username;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	
+	public String getPassword() {
+		return password;
+	}
+
+	
+	public String getSalt() {
+		return salt;
+	}
+
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	  
+	  
+
+	
 
 }
