ee3e22573e1b70df7057a182e3f6d2d1c0c65f88
Pedro Silva <pedrbsilva@gmail.com>
2018-08-02 15:39:24 +0100

    criação de classe abstracta navigator

---
+++ src/main/java/bean/navigator.java
@@ -0,0 +1,44 @@
+package bean;
+
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+@SessionScoped
+public abstract class navigator implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5158381528968071445L;
+	/**
+	 * 
+	 */
+	
+	
+	
+	private static long requerimentoId;
+	private static long anuncioId;
+	private static String lastPage;
+	
+	
+	public static long getRequerimentoId() {
+		return requerimentoId;
+	}
+	public static void setRequerimentoId(long requerimentoId) {
+		navigator.requerimentoId = requerimentoId;
+	}
+	public static long getAnuncioId() {
+		return anuncioId;
+	}
+	public static void setAnuncioId(long anuncioId) {
+		navigator.anuncioId = anuncioId;
+	}
+	public static String getLastPage() {
+		return lastPage;
+	}
+	public static void setLastPage(String lastPage) {
+		navigator.lastPage = lastPage;
+	}
+	
+	
+}
