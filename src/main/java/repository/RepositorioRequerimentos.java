10a0d4cd402295d38cb73a1365f80f3b92945f4e
Pedro Silva <pedrbsilva@gmail.com>
2018-08-03 10:26:58 +0100

    criada query para requerimentos por processar

--- src/main/java/repository/RepositorioRequerimentos.java
+++ src/main/java/repository/RepositorioRequerimentos.java
@@ -1,6 +1,8 @@
 package repository;
 
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
@@ -15,10 +17,17 @@ public class RepositorioRequerimentos extends EntityRepository <Requerimento>{
 	protected void loadFromDB() {
 		localList = em.createQuery("SELECT e FROM Requerimento e").getResultList();
 	}
+	
 
 	@Override
 	public void updateLocalList() {
 		loadFromDB();
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected List<Requerimento> porProcessar(){
+		return  (em.createQuery("SELECT e FROM REQUERIMENTO e WHERE REQUERIMENTO.estado = 'Por Processar'").getResultList());
+		
 	}
 	
 	
