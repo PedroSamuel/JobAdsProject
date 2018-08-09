69843af9daeeda31b45aaf4dd04201151081e91a
anterosequeira <anterosequeira3@gmail.com>
2018-08-09 12:46:23 +0100

    comitado
    mudancas

--- src/main/java/control/ControlAnuncio.java
+++ src/main/java/control/ControlAnuncio.java
@@ -25,6 +25,7 @@ public class ControlAnuncio {
 	private ControlAnuncioPlataforma APControl;
 	
 	public void criarAnuncio(Anuncio anuncio) {
+		anuncio.temTarefa();
 		dbA.createEntity(anuncio);
 	}
 	
